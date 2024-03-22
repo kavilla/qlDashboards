c2a34cff2b6669e1e3c089655fedcbd29ddbfd23
Kawika Avilla <kavilla414@gmail.com>
2024-03-22 19:33:48 +0000

    enable config
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,14 @@
-import { PluginInitializerContext } from '../../../src/core/server';
+import { PluginConfigDescriptor, PluginInitializerContext } from '../../../src/core/server';
 import { QlDashboardsPlugin } from './plugin';
+import { configSchema, ConfigSchema } from '../common/config';
 
 // This exports static code and TypeScript types,
 // as well as, OpenSearch Dashboards Platform `plugin()` initializer.
+
+export const config: PluginConfigDescriptor<ConfigSchema> = {
+  exposeToBrowser: {},
+  schema: configSchema,
+};
 
 export function plugin(initializerContext: PluginInitializerContext) {
   return new QlDashboardsPlugin(initializerContext);
