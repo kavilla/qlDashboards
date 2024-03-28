b773650282c046bffb1f9db9bdb9c9b164f0e9be
Kawika Avilla <kavilla414@gmail.com>
2024-03-28 12:41:38 +0000

    use data frames
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -6,8 +6,16 @@ import {
   ResponseError,
 } from '../../../../src/core/server';
 import { ISearchStrategy } from '../../../../src/plugins/data/server';
+import {
+  IDataFrameResponse,
+  IOpenSearchDashboardsSearchRequest,
+} from '../../../../src/plugins/data/common';
 
-export function defineRoutes(logger: Logger, router: IRouter, searchStrategy: ISearchStrategy) {
+export function defineRoutes(
+  logger: Logger,
+  router: IRouter,
+  searchStrategy: ISearchStrategy<IOpenSearchDashboardsSearchRequest, IDataFrameResponse>
+) {
   router.post(
     {
       path: `/api/ql/search`,
@@ -19,19 +27,21 @@ export function defineRoutes(logger: Logger, router: IRouter, searchStrategy: IS
       },
     },
     async (context, req, res): Promise<IOpenSearchDashboardsResponse<any | ResponseError>> => {
-      const queryRes: any = await searchStrategy.search(context, req as any, {});
-      if (queryRes.success) {
+      try {
+        const queryRes: IDataFrameResponse = await searchStrategy.search(context, req as any, {});
         const result: any = {
           body: {
             ...queryRes,
           },
         };
         return res.ok(result);
+      } catch (err) {
+        logger.error(err);
+        return res.custom({
+          statusCode: 500,
+          body: err,
+        });
       }
-      return res.custom({
-        statusCode: queryRes.data.statusCode || queryRes.data.status || 500,
-        body: queryRes.data.body || queryRes.data.message || '',
-      });
     }
   );
 }
